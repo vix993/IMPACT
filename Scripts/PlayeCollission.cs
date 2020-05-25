13154c6c47c6c7b41e21d9a4f91125d1b3cbfe38
vix993 <52217652+vix993@users.noreply.github.com>
2020-05-25 14:14:59 -0300

    Add files via upload

---
+++ Scripts/PlayeCollission.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayeCollission : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo){
+
+        if(collisionInfo.collider.tag == "ObstacleTag")
+        {
+            movement.enabled = false; 
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+}
